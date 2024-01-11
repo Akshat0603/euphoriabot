d72915516c96f5898ca0b4aa0d3a96dd8e7e713e
Akshat <akshatb2008@gmail.com>
2024-01-11 16:57:58 +0530

    Error logging

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = require("fs");
 const client_1 = __importDefault(require("./client"));
 const partials_1 = require("./prebuilts/partials");
+process.on("uncaughtException", (error) => {
+    const date = new Date().toString();
+    (0, fs_1.appendFileSync)(`error.log`, `\n${date}\nUnhandled Exception: ${error.message}\n${error.stack}\n\n`);
+    process.exit(1);
+});
 new client_1.default({ intents: 46975, partials: partials_1.myPartials }).init(__dirname);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
+import { appendFileSync } from "fs";
 import myClient from "./client";
 import { myPartials } from "./prebuilts/partials";
+
+process.on("uncaughtException", (error) => {
+	const date = new Date().toString();
+	appendFileSync(
+		`error.log`,
+		`\n${date}\nUnhandled Exception: ${error.message}\n${error.stack}\n\n`
+	);
+	process.exit(1);
+});
 
 new myClient({ intents: 46975, partials: myPartials }).init(__dirname);
