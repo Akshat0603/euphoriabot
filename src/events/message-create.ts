90db3432a5f79f3dab269a734a6c61c6e2ffd4f3
Akshat <akshatb2008@gmail.com>
2024-01-27 20:29:56 +0530

    removed fun responses

--- src/events/message-create.ts
+++ src/events/message-create.ts
@@ -6,15 +6,6 @@ const format = `tellraw @a ["",{"text":"[Discord] ","bold":true,"color":"blue"},
 const usernameRegex = /{username}/;
 const messageRegex = /{message}/;
 
-const funReplyArray: string[] = [
-	"Why?",
-	"No.",
-	"What do you want?",
-	"Fuck you.",
-	"euphoria.exe stokpped working.",
-	"Roger that!",
-];
-
 export const event: eventType = {
 	name: "messageCreate",
 	execute: async (client: myClient, message: Message) => {
@@ -56,10 +47,10 @@ export const event: eventType = {
 			);
 
 			await client.CMP.send("send command", [messageContent]);
-		} else if (message.content.endsWith("?") || message.mentions.has(client.user!)) {
-			await message.reply({
-				content: funReplyArray[Math.round(Math.random() * (funReplyArray.length - 1))],
-			});
+		}
+		// fun responses
+		else if (message.content.toLowerCase() === "lol") {
+			await message.reply({ content: message.content });
 		}
 	},
 };
