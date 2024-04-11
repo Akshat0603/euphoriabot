d8ba6786034dd0c467b6f1e42c87270f464ff230
Akshat <akshatb2008@gmail.com>
2024-04-11 11:18:11 +0530

    S2 changes built

--- dist/events/message-create.js
+++ dist/events/message-create.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.event = void 0;
+const discord_js_1 = require("discord.js");
+const fs_1 = require("fs");
 const format = `tellraw @a ["",{"text":"[Discord] ","bold":true,"color":"blue"},{"text":"{username}: ","color":"gray"},{"text":"{message}","color":"white"}]`;
 const usernameRegex = /{username}/;
 const messageRegex = /{message}/;
@@ -40,6 +42,39 @@ exports.event = {
             messageContent = messageContent.replace(usernameRegex, username);
             messageContent = messageContent.replace(messageRegex, message.content.replaceAll('"', '\\"'));
             await client.CMP.send("send command", [messageContent]);
+        }
+        else if (message.channelId === client.channelMemberListID) {
+            let name = message.content;
+            await message.delete();
+            if (name.split(" ").length > 1)
+                return;
+            const messageMember = message.member;
+            if (messageMember.roles.cache.has(client.memberRoleID) || !messageMember.roles.cache.has(client.S1memberRoleID))
+                return;
+            //await messageMember.setNickname(name);
+            await messageMember.roles.add(client.memberRoleID);
+            // adding them to member list channel
+            var memberList = JSON.parse((0, fs_1.readFileSync)("./storage/member-list.json").toString());
+            memberList.push(messageMember.id);
+            (0, fs_1.writeFileSync)("./storage/member-list.json", JSON.stringify(memberList));
+            const mChannel = client.channels.cache.get(client.channelMemberListID);
+            // impossible error check
+            if (!mChannel || mChannel.type !== discord_js_1.ChannelType.GuildText) {
+                console.log(`[EVENTS] An error occured in "message-create"! Code #4`);
+                return;
+            }
+            var content = "";
+            var members = message.guild.roles.cache.get(client.memberRoleID).members;
+            members = members.sort((a, b) => (a.nickname || a.displayName).localeCompare(b.nickname || b.displayName));
+            for (const member of members) {
+                content =
+                    content +
+                        `\n- ${(member[1].nickname ? member[1].nickname : member[1].displayName).replace("_", "\\_")}`;
+            }
+            const messages = await mChannel.messages.fetch();
+            const IDKmessage = messages.get(client.messageMemberListID);
+            await IDKmessage?.edit({ content });
+            await mChannel.setTopic(`${members.size} members!`);
         }
         // fun responses
         else if (message.content.toLowerCase() === "lol") {
